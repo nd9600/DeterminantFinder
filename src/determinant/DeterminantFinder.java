818902afc630d8cf30cf03c029bbaa7242c5499f
Nathan Douglas <nd9600@gmail.com>
2015-10-28 20:46:31 +0000

    Initial commit

---
+++ src/determinant/DeterminantFinder.java
@@ -0,0 +1,18 @@
+package determinant;
+
+public class DeterminantFinder {
+	
+	public void main(String[] args){
+		Double[][] inputMatrix = {{}};
+		
+		Double determinant = getDeterminant(inputMatrix);
+		
+		System.out.println("Determinant is: " + determinant);
+		
+	}
+	
+	private Double getDeterminant(Double[][] matrix){
+		
+		return 0.0;
+	}
+}
