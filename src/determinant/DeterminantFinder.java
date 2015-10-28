03f03dbad694b912909974daf9ff29d583af6f49
Nathan Douglas <nd9600@gmail.com>
2015-10-28 22:25:40 +0000

    Works; no optimisations yet

--- src/determinant/DeterminantFinder.java
+++ src/determinant/DeterminantFinder.java
@@ -1,9 +1,12 @@
 package determinant;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DeterminantFinder {
 	
 	public static void main(String[] args){
-		Double[][] inputMatrix = {{1.0 , 2.0}, {3.0 , 4.0 }};
+		Double[][] inputMatrix = {{1.0 , 2.0, 3.0}, {4.0 , 1.0, 0.0}, {7.0 , 3.0, 9.0}};
 		
 		Double determinant = getDeterminant(inputMatrix);
 		
@@ -19,6 +22,11 @@ public class DeterminantFinder {
 	}
 	
 	private static Double getDeterminant(Double[][] matrix){
+		
+		System.out.println("");
+		System.out.println("Input matrix is");
+		for (Double[] row : matrix)
+		    System.out.println(Arrays.toString(row));
 		
 		int sizeOfMatrix = matrix[0].length;
 		
@@ -47,12 +55,11 @@ public class DeterminantFinder {
 		
 		//Else, if not 2x2
 		
-		Double tempDeterminant = 0.0;
+		Double determinant = 0.0;
 		
-		//Loops through every element in the first row
+		//Loops through every element in the first row (for now)
 		for (int i=0; i<1; i++){
 			for (int j=0; j<sizeOfMatrix; j++){
-				System.out.println("tempDeterminant is: " + tempDeterminant);
 				
 				int actualRowIndex = i + 1;
 				int actualColumnIndex = j + 1;
@@ -61,25 +68,64 @@ public class DeterminantFinder {
 				int sign = (int) Math.pow(-1.0, actualRowIndex+actualColumnIndex);
 				Double relevantElement = matrix[i][j];
 				
-				//Create the new matrix to find the determinant of by removing the row and column the element is in
-				Double[][] newMatrix = matrix;
+				System.out.println("");
+				System.out.println("#############################################");
 				
-				//Calculate the determinant of the new smaller matrix, multiply it by the relevant elemant and the sign, and add the product to the temporary 
+				//Create the new matrix to find the determinant of by removing the row and column the element is in
+				ArrayList<ArrayList<Double>> newMatrixArrayList = new ArrayList<ArrayList<Double>>();
+				
+				//Loops through every row
+				for (int k=0; k<sizeOfMatrix; k++){
+					
+					ArrayList<Double> tempRowMatrixArrayList = new ArrayList<Double>();
+					//Loops through every column
+					for (int l=0; l<sizeOfMatrix; l++){
+						
+						//Checks if we aren't in the same row or column as the relevant element
+						if (! ((k == i) || (l == j)) ){	
+							//Adds the new element to the temporary row if not
+							Double rowElement = matrix[k][l];
+							tempRowMatrixArrayList.add(rowElement);
+						}
+					}
+					
+					//Adds the temporary row to the new smaller matrix if it isn't empty
+					if (! tempRowMatrixArrayList.isEmpty()){
+						newMatrixArrayList.add(tempRowMatrixArrayList);
+					}					
+					
+				}
+				
+				//Converts the ArrayList to an array
+				Double[][] newMatrix = new Double[newMatrixArrayList.size()][];
+				for (int iterator = 0; iterator < newMatrixArrayList.size(); iterator++) {
+				    ArrayList<Double> row = newMatrixArrayList.get(iterator);
+				    newMatrix[iterator] = row.toArray(new Double[row.size()]);
+				}
+				
+				System.out.println("newMatrix is: ");
+				for (Double[] row : newMatrix)
+				    System.out.println(Arrays.toString(row));
+				System.out.println("");
+				System.out.println("##");
+								
+				//Calculate the determinant of the new smaller matrix, multiplies it by the relevant element and the sign, and adds the product to the determinant
 				Double newMatrixDeterminant =  getDeterminant(newMatrix);
 				Double actualNewMatrixDeterminant = sign * relevantElement * newMatrixDeterminant;
-				tempDeterminant = tempDeterminant + actualNewMatrixDeterminant;
 				
 				System.out.println("");
-				System.out.println("###############");
 				System.out.println("sign is: " + sign);
 				System.out.println("relevantElement is: " + relevantElement);
-				System.out.println("newMatrix is: " + newMatrix);
 				System.out.println("newMatrixDeterminant is: " + newMatrixDeterminant);
 				System.out.println("actualNewMatrixDeterminant is: " + actualNewMatrixDeterminant);
-				System.out.println("tempDeterminant is: " + tempDeterminant);
+				System.out.println("previousDeterminant is: " + determinant);
+				
+				determinant = determinant + actualNewMatrixDeterminant;
+				
+				System.out.println("new determinant is: " + determinant);
 			}
 		}
 		
-		return tempDeterminant;
+		return determinant;
 	}
 }
